c313bff68dee606e71552d8315e88e896350204f
Алексей Кирютин <Aleksey-Y-Kiryutin@yandex.ru>
2023-08-09 20:07:21 +0300

    add: добавил вывод представительства админам

--- src/handlers/report_problems.py
+++ src/handlers/report_problems.py
@@ -6,17 +6,35 @@ from aiogram.dispatcher import FSMContext
 
 from data.answers import (
     default_answer_message,
+    starting_answer_message,
     problem_reporting_answer_message,
     problem_category_selection_answer_message
 )
 from data.categiries import ALLOWED_PROBLEM_CATEGORIES
-from data.entities import Admin
+from data.entities import User, Admin
 from data.states import ReportProblem
 
 logger = logging.getLogger()
 
 
 async def report_problem(message: types.Message, state: FSMContext):
+    reporter_id = str(message.from_user.id)
+    reporter = User.get_or_none(User.telegram_id == reporter_id)
+
+    if not reporter:
+        await message.answer(**default_answer_message(
+            'Упс! Похоже вы не зарегистрированы в БД. '
+            'Необходимо заново выполнить команду /start или обратиться к администраторам бота'
+        ))
+        await state.finish()
+        return
+
+    if not reporter.representative_office_number:
+        await message.answer(**starting_answer_message('Сначала необходимо выбрать представительство'))
+        await state.finish()
+        return
+
+    await state.update_data(reporter_office_number=reporter.representative_office_number)
     await state.set_state(ReportProblem.WAITING_CATEGORY_SELECTION)
     await message.answer(**problem_reporting_answer_message())
 
@@ -47,9 +65,13 @@ async def register_report(message: types.Message, state: FSMContext):
     state_data = await state.get_data()
     problem_category = state_data['problem_category']
     report_messages = state_data['messages']
+    office_number = state_data['reporter_office_number']
 
     for admin in Admin.select():
-        await message.bot.send_message(admin.telegram_id, f'Поступило сообщение о проблеме на тему {problem_category}')
+        await message.bot.send_message(
+            admin.telegram_id,
+            f'Поступило сообщение от представительства №{office_number} о проблеме на тему "{problem_category}"'
+        )
         await forward_messages_to_user(report_messages, admin.telegram_id)
 
     logger.info(f'Пользователь {message.from_user.id} сообщил о проблеме "{problem_category}"')
